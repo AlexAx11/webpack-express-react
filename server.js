f3e4b036bbee7e3765a10c2e7b28d9c16f0fb43d
AlexAx11 <alexaxyanov@gmail.com>
2023-08-03 16:09:01 +0300

    install cors. get cus works

---
+++ server.js
@@ -0,0 +1,42 @@
+// Create express app
+var express = require("express")
+var app = express()
+var db = require("./database.js")
+var cors = require('cors');
+
+
+
+// Server port
+var HTTP_PORT = 8000 
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+// Root endpoint
+app.get("/", (req, res, next) => {
+    res.json({"message":"Ok"})
+});
+
+app.use(cors());
+
+// Insert here other API endpoints
+app.get("/api/customers", (req, res, next) => {
+    var sql = "select * from customer"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404);
+});
+
